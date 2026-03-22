ececd11385e4e0a2924f60806f5040c0bd701eeb
Brahim Ouhammou <bouhammo@c1r2p8.1337.ma>
2026-03-22 11:09:06 +0100

    change structure'

--- test/train.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-print(f"path file is { os.path.abspath(__file__)} ")
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  
-print(f"path file is { os.path.dirname(os.path.dirname(os.path.abspath(__file__)))} ")
-
-print(BASE_DIR)
